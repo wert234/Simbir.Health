13fb10e9928a08570c46fa48ca66d0964cd9be64
Vladlen <55331530+wert234@users.noreply.github.com>
2024-09-22 17:09:04 +0300

    Add IRepository

---
+++ src/Service/Sherad/Sherad.Application/Repositories/IRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sherad.Application.Repositories
+{
+    public interface IRepository<T, TKey>
+        where T : notnull 
+        where TKey : notnull
+    {
+        Task<IEnumerable<T>> GetAllAsync();
+        Task<T> GetAsync(TKey id);
+        Task<T> AddAsync(T entity);
+        Task<T> UpdateAsync(T entity);
+        Task<T> DeleteAsync(TKey id);
+
+        Task SaveAsync(CancellationToken token = default);
+    }
+}
