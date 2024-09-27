23335e63cc06ea48f91212bfcff3a870b0a487f2
Vladlen <55331530+wert234@users.noreply.github.com>
2024-09-27 22:54:51 +0300

    Add HospitalRepository

---
+++ src/Service/Microservices/Hospital/Hospital.Infastructure/Repositories/HospitalRepository.cs
@@ -0,0 +1,57 @@
+﻿using Hospital.Infastructure.Data.DbContexts;
+using Microsoft.EntityFrameworkCore;
+using Sherad.Application.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hospital.Infastructure.Repositories
+{
+    public class HospitalRepository : IRepository<Hospital.Domain.Entitys.Hospital, Guid>
+    {
+
+        private readonly HospitalDbContext _hospitalDbContext;
+
+
+        public HospitalRepository(HospitalDbContext hospitalDbContext)
+        {
+            _hospitalDbContext = hospitalDbContext;
+        }
+
+
+        public async Task AddAsync(Domain.Entitys.Hospital entity)
+        {
+            await _hospitalDbContext.AddAsync(entity);
+            await _hospitalDbContext.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(Guid id)
+        {
+             _hospitalDbContext.Remove(await GetAsync(id));
+            await _hospitalDbContext.SaveChangesAsync();
+        }
+
+        public async Task<IEnumerable<Domain.Entitys.Hospital>> GetAllAsync()
+        {
+            return await _hospitalDbContext.Hospitals.ToArrayAsync();
+        }
+
+        public async Task<Domain.Entitys.Hospital> GetAsync(Guid id)
+        {
+            return await _hospitalDbContext.Hospitals.FirstOrDefaultAsync(hospital => hospital.Id == id);
+        }
+
+        public async Task SaveAsync(CancellationToken token = default)
+        {
+            await _hospitalDbContext.SaveChangesAsync(token);
+        }
+
+        public async Task UpdateAsync(Domain.Entitys.Hospital entity)
+        {
+            _hospitalDbContext.Hospitals.Update(entity);
+            await _hospitalDbContext.SaveChangesAsync();
+        }
+    }
+}
